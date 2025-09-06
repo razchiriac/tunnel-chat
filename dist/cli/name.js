2c72fb277de27c029bba7046d8c036ced4ec4845
Raz <raz.chiriac@gmail.com>
2025-09-05 22:06:14 -0400

    cleanup

--- dist/cli/name.js
+++
@@ -1,7 +0,0 @@
-const A = ['brisk', 'calm', 'cosmic', 'dapper', 'eager', 'groovy', 'mellow', 'nimble', 'plucky', 'quirky', 'snug', 'spry', 'zesty'];
-const B = ['apple', 'bread', 'cloud', 'ember', 'falcon', 'maple', 'meadow', 'pepper', 'pixel', 'rocket', 'saffron', 'thunder', 'voyage'];
-const C = ['king', 'queen', 'rider', 'scout', 'smith', 'sage', 'pilot', 'keeper', 'oracle', 'weaver', 'ranger', 'scribe', 'tinker'];
-export function autoName() {
-    const r = (n) => Math.floor(Math.random() * n);
-    return `${A[r(A.length)]}-${B[r(B.length)]}-${C[r(C.length)]}`;
-}
